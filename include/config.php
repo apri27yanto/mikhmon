be67314f98b51ed0daf2101dcf4adae804990e50
apri <63585301+apri27yanto@users.noreply.github.com>
2023-08-19 19:16:48 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<ratech','mikhmon>|>qnGma5SaamFkaw==');
+
+$data['rahotspot'] = array ('1'=>'rahotspot!10.25.19.65','rahotspot@|@ratech','rahotspot#|#qnGma5SaamFkaw==','rahotspot%rahotspot','rahotspot^Ra WIFI','rahotspot&Rp','rahotspot*10','rahotspot(1','rahotspot)','rahotspot=10','rahotspot@!@disable');
